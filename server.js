cea7d051069653df9db8838d0bcb1a186491d321
alirueter <rueter.alison@gmail.com>
2020-08-07 08:41:06 -0500

    added html routes

---
+++ server.js
@@ -0,0 +1,23 @@
+//initiate express
+const express = require('express');
+const app = express();
+//declare port
+const PORT = process.env.PORT || 3001;
+//declare variables for routes
+const htmlRoutes = require('./routes/htmlRoutes');
+
+//middleware to connect stylesheets
+app.use(express.static('public'));
+//parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+//parse incoming JSON data
+app.use(express.json());
+
+//get routes
+app.use('/', htmlRoutes);
+
+
+//keep at end of file
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
