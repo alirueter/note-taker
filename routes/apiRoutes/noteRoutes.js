044ceacec60af6518f55bf97c89e3326ba42a769
alirueter <rueter.alison@gmail.com>
2020-08-09 16:50:15 -0500

    updated function name to try to fix bug

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const notes = require('../../db/db.json');
-const {createNote, findById} = require('../../lib/notes');
+const {createNewNote, findById} = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     let results = notes;
@@ -21,7 +21,7 @@ router.post('/notes', (req,res) => {
     console.log(req.body);
 
     req.body.id = notes.length + 1;
-    const newNotes = createNote(req.body, notes);
+    const newNotes = createNewNote(req.body, notes);
     res.json(newNotes);
 
 });
